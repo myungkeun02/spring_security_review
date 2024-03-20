aa4babcdf2c2ad281e6b3f6025072637260507c9
myungkeun <audrms3568@gmail.com>
2024-03-20 21:52:38 +0900

    database connect

---
+++ src/main/java/org/myungkeun/security_study/SecurityStudyApplication.java
@@ -0,0 +1,13 @@
+package org.myungkeun.security_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityStudyApplication.class, args);
+    }
+
+}
